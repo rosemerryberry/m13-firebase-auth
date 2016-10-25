e6148677152f961266693b2646c13a08ef45bfc7
rosemerryberry <adamsrc@uw.edu>
2016-10-25 09:22:22 -0700

    added sign up form wo button

--- exercise-1/js/authenticate.js
+++ exercise-1/js/authenticate.js
@@ -2,10 +2,29 @@
 $(function() {
 
     // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyBkOERDxof4n4RN0iyzetTzHq1pju_a-NM",
+        authDomain: "fir-2-d6e32.firebaseapp.com",
+        databaseURL: "https://fir-2-d6e32.firebaseio.com",
+        storageBucket: "fir-2-d6e32.appspot.com",
+        messagingSenderId: "311225974804"
+      };
+      firebase.initializeApp(config);
+
+      var auth = firebase.auth();
 
     // Sign Up: Function to create account on firebase, then redirect to index.html
     var signUp = function() {
-        // Get email, password, and display name
+        
+        // Get email and password from a form
+        var email = $('#email').val();
+        var password = $("#password").val();
+        var displayName = $("#displayName").val();
+
+        // Create a user using the values from your form
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
+            // Do something after the user has been created
+        });
 
 
         // Create user, then set the user's display name
